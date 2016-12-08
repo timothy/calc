c761a8a7c6486dcd3804fdd569f982038cdd3337
timothy <tabradford@liberty.edu>
2016-12-08 15:20:33 -0500

    added both totals for user to see at bottom of page. Changed calc structure a bit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {} from ''
 import {DayTimeTracker, TotalTime} from "./types";
 
 @Component({
@@ -9,21 +8,21 @@ import {DayTimeTracker, TotalTime} from "./types";
 })
 export class AppComponent {
 
-   readonly DOW: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-   readonly week: number = 7;
-   readonly milliMin: number = 60 * 1000;
-   readonly milliHour: number = 60 * this.milliMin;
-   readonly totalTime: number = 40;// this is total time in millisecond
+  readonly DOW: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  readonly week: number = 7;
+  readonly milliMin: number = 60 * 1000;
+  readonly milliHour: number = 60 * this.milliMin;
+  readonly totalTime: number = 40;// this is total time in millisecond
 
   //---used in the view---
-   days: Array<DayTimeTracker> = [];
-   btnActive: Object = {decimal: '', min: 'active'};
-   time: TotalTime = {decimal: 40, hours: 40, min: 0};
-   calcType: string = 'min';//or decimal
+  days: Array<DayTimeTracker> = [];
+  btnActive: Object = {decimal: '', min: 'active'};
+  time: TotalTime = {decimal: 40, hours: 40, min: 0};
+  calcType: string = 'min';//or decimal-- using strings instead of boolean because I may add other calculation types in the future
 
   constructor() {
     for (let i = 0; i < this.week; i++) {
-      this.days.push({day: this.DOW[i], hours: 0, min: 0, decimalTime: 0,overflow:0});
+      this.days.push({day: this.DOW[i], hours: 0, min: 0, decimalTime: 0, overflow: 0});
     }
   }
 
@@ -31,7 +30,7 @@ export class AppComponent {
    * this will calculate the remaining time based on current user input
    */
   calcTime() {
-    let hrTime: number =this.totalTime * this.milliHour;
+    let hrTime: number = this.totalTime * this.milliHour;
     let dTime: number = this.totalTime;
 
     for (let c = 0; c < this.week; c++) {
@@ -40,26 +39,29 @@ export class AppComponent {
       hrTime -= this.days[c].min * this.milliMin;
     }
 
-   // hrTime = dTime * this.milliHour;
-
-  this.time = {
-    decimal: dTime,
-    hours: Math.floor(hrTime / this.milliHour),
-    min: Math.floor((hrTime % this.milliHour) / this.milliMin)
-  };
-
+    this.time = {
+      decimal: dTime,
+      hours: Math.floor(hrTime / this.milliHour),
+      min: Math.floor((hrTime % this.milliHour) / this.milliMin)
+    };
+    this.ConvertTime();
   }
 
   /**
    * this will change the type of calculation to be displayed
    * @param newVal:string == the new type of calculation
    */
-  changeCalc(newVal:string) {
-    if(newVal !== this.calcType){
+  changeCalc(newVal: string) {
+    if (newVal !== this.calcType) {
       //set btn active
       this.calcType = newVal;
 
-      this.mergeTime();
+      // This will merge decimal time and regular time together.
+      if (this.calcType === 'min') {
+        this.mergeDecToMin();
+      } else if (this.calcType === 'decimal') {
+        this.mergeMinToDec();
+      }
 
       for (let prop in this.btnActive) {
         this.btnActive[prop] = '';
@@ -69,34 +71,49 @@ export class AppComponent {
   }
 
   /**
-   * This will merge decimal time and regular time together.
+   * This will merge decimal time and regular time together based on input.
+   * default merge time from current tab to other tab.
    */
-  mergeTime(){
-    if(this.calcType === 'min'){
-      let hrTime: number = this.totalTime * this.milliHour;
-      for (let c = 0; c < this.week; c++) {
-        this.days[c].overflow = this.days[c].decimalTime;
-        this.days[c].hours = Math.floor(this.days[c].decimalTime);
-        this.days[c].min = Math.floor(this.days[c].decimalTime*60)%60;
-        hrTime -= this.days[c].hours * this.milliHour;
-        hrTime -= this.days[c].min * this.milliMin;
+  ConvertTime(curTabToAllTab: boolean = true) {
+    if (curTabToAllTab) {
+      if (this.calcType === 'min') {
+        this.mergeMinToDec();
+      } else if (this.calcType === 'decimal') {
+        this.mergeDecToMin();
       }
-      this.time.hours = Math.floor(hrTime / this.milliHour);
-      this.time.min = Math.floor((hrTime % this.milliHour) / this.milliMin);
-
-    }else if (this.calcType === 'decimal'){
-      let dTime: number = this.totalTime;
-
-      for (let c = 0; c < this.week; c++) {
-        if(this.days[c].overflow){
-          this.days[c].decimalTime = this.days[c].overflow
-        } else{
-          this.days[c].decimalTime = ((this.days[c].hours * this.milliHour)+(this.days[c].min * this.milliMin)) / this.milliHour;
-        }
-        dTime -= this.days[c].decimalTime;
+    } else {
+      if (this.calcType === 'min') {
+        this.mergeDecToMin();
+      } else if (this.calcType === 'decimal') {
+        this.mergeMinToDec();
       }
-      this.time.decimal = dTime;
     }
+  }
+
+  mergeDecToMin() {
+    let hrTime: number = this.totalTime * this.milliHour;
+    for (let c = 0; c < this.week; c++) {
+      this.days[c].overflow = this.days[c].decimalTime;
+      this.days[c].hours = Math.floor(this.days[c].decimalTime);
+      this.days[c].min = Math.floor(this.days[c].decimalTime * 60) % 60;
+      hrTime -= this.days[c].hours * this.milliHour;
+      hrTime -= this.days[c].min * this.milliMin;
+    }
+    this.time.hours = Math.floor(hrTime / this.milliHour);
+    this.time.min = Math.floor((hrTime % this.milliHour) / this.milliMin);
+  }
+
+  mergeMinToDec() {
+    let dTime: number = this.totalTime;
+    for (let c = 0; c < this.week; c++) {
+      if (this.days[c].overflow) {
+        this.days[c].decimalTime = this.days[c].overflow
+      } else {
+        this.days[c].decimalTime = ((this.days[c].hours * this.milliHour) + (this.days[c].min * this.milliMin)) / this.milliHour;
+      }
+      dTime -= this.days[c].decimalTime;
+    }
+    this.time.decimal = dTime;
   }
 
 }
