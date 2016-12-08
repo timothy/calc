b0a2bf76cadbe85f802da6f7bada63841fba7961
timothy <tabradford@liberty.edu>
2016-12-08 16:33:44 -0500

    Finished logic for new page layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,6 @@ export class AppComponent {
 
   //---used in the view---
   days: Array<DayTimeTracker> = [];
-  btnActive: Object = {decimal: '', min: 'active'};
   time: TotalTime = {decimal: 40, hours: 40, min: 0};
 
   constructor() {
@@ -25,44 +24,39 @@ export class AppComponent {
     }
   }
 
-  /**
-   * this will calculate the remaining time based on current user input
-   */
-  calcTime() {
-    let hrTime: number = this.totalTime * this.milliHour;
+  calcDecTime() {
     let dTime: number = this.totalTime;
+    let hrTime: number = this.totalTime * this.milliHour;
 
     for (let c = 0; c < this.week; c++) {
       dTime -= this.days[c].decimalTime;
+
+      this.days[c].hours = Math.floor(this.days[c].decimalTime);
+      this.days[c].min = Math.floor(this.days[c].decimalTime * 60) % 60;
       hrTime -= this.days[c].hours * this.milliHour;
       hrTime -= this.days[c].min * this.milliMin;
-    }
-
-    this.crossCalc();
-  }
-
-  crossCalc() {
-    let dTime: number = this.totalTime;
-    let hrTime: number = this.totalTime * this.milliHour;
-    for (let c = 0; c < this.week; c++) {
-      if(this.days[c].decimalTime){
-        this.days[c].overflow = this.days[c].decimalTime;
-        this.days[c].hours = Math.floor(this.days[c].decimalTime);
-        this.days[c].min = Math.floor(this.days[c].decimalTime * 60) % 60;
-        hrTime -= this.days[c].hours * this.milliHour;
-        hrTime -= this.days[c].min * this.milliMin;
-      }else {
-        if (this.days[c].overflow) {
-          this.days[c].decimalTime = this.days[c].overflow
-        } else {
-          this.days[c].decimalTime = ((this.days[c].hours * this.milliHour) + (this.days[c].min * this.milliMin)) / this.milliHour;
-        }
-        dTime -= this.days[c].decimalTime;
-      }
     }
     this.time.decimal = dTime;
     this.time.hours = Math.floor(hrTime / this.milliHour);
     this.time.min = Math.floor((hrTime % this.milliHour) / this.milliMin);
+  }
+
+  CalcTime() {
+    let dTime: number = this.totalTime;
+    let hrTime: number = this.totalTime * this.milliHour;
+
+    for (let c = 0; c < this.week; c++) {
+      this.days[c].decimalTime = ((this.days[c].hours * this.milliHour) + (this.days[c].min * this.milliMin)) / this.milliHour;
+      hrTime -= this.days[c].hours * this.milliHour;
+      hrTime -= this.days[c].min * this.milliMin;
+      dTime -= this.days[c].decimalTime;
+    }
+
+    this.time = {
+      hours: Math.floor(hrTime / this.milliHour),
+      min: Math.floor((hrTime % this.milliHour) / this.milliMin),
+      decimal: dTime
+    };
   }
 
 }
